7b4190c399249c6455d613ff788b07cd97070423
anon.js <anon@bible.ac.kr>
2026-02-25 07:39:56 +0000

    fix: Add NextResponse

--- proxy.ts
+++ proxy.ts
@@ -1,9 +1,10 @@
 import { auth } from "@/auth";
+import { NextResponse } from "next/server";
 
 export default auth((req) => {
   if (!req.auth && req.nextUrl.pathname !== "/login" && req.nextUrl.pathname !== "/signup") {
     const newUrl = new URL("/login", req.nextUrl.origin);
-    return Response.redirect(newUrl);
+    return NextResponse.redirect(newUrl);
   }
 });
 
